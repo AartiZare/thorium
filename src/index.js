d974d09e146bfa898da3f2b186d8afee4e599410
Aarti zare <artizare140@gmail.com>
2022-03-06 00:59:46 +0530

    a

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 const express = require('express');
+const moment = require("moment")
+//const requestIp = require("request-ip")
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
+//app.use(requestIp.mw([attributeName , 'customIp' ]))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+// app.use(function(req, res, next){
+//     let date = moment().format('MMMM DO YYYY, h:mm:ss a')
+//     let ip = req.customIp;
+//   //  console.log(date, ip)
+//     next()
+
+//     res.send({msg:ip}, {msg:date})
+// })
+
+mongoose.connect("mongodb+srv://AartiZare:aartizare@cluster0.l0uzu.mongodb.net/Aarti-Db?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
+
+const GlobalMW = function(req, res, next){
+    const Time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/,'')
+    const Request = req.originalUrl
+    const IP = req.ip
+    console.log(Time , Request, IP)
+}
+
+app.use(GlobalMW)
 
 app.use('/', route);
 
